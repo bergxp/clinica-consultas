9e60435c93c55a183c04717692e6a958ab4bbe82
bergxp <berg215@hotmail.com>
2025-08-06 09:24:29 -0300

    ajuste page paciente para recepcao

--- src/app/recepcao/page.tsx
+++ src/app/recepcao/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-export default function Paciente() {
+export default function Recepcao() {
 
   type Paciente = { id: number; nome: string };
   const [pacientes, setPacientes] = useState<Paciente[]>([]);
